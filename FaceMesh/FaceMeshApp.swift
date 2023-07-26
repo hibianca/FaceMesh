58446ba3fffa9bc0db0c795e8644adce3c9ca9bd
hibianca <heybiancalima@gmail.com>
2023-07-26 10:54:41 -0300

    Initial Commit

---
+++ FaceMesh/FaceMeshApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceMeshApp.swift
+//  FaceMesh
+//
+//  Created by Bianca Nathally Bezerra de Lima on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct FaceMeshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
